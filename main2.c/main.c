5478ad574621c5cfe7ea32e7fba74f206dd26674
AdrienBoiseau <adrien-97@hotmail.fr>
2015-08-30 11:10:58 +0200

    Initial Commit

---
+++ main2.c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  main2.c
+//
+//  Created by Adrien Boiseau on 30/08/2015.
+//  Copyright (c) 2015 Adrien Boiseau. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
