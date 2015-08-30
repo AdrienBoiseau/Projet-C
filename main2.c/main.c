e9d3850ee16b6fad58daf2441ba9be0106e4752f
Paul Boiseau <paulboiseau@gmail.com>
2015-08-30 11:24:04 +0200

    ajout de la fonction addition

--- main2.c/main.c
+++ main2.c/main.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
+
 int main(void){
     int adrien_age;
     adrien_age=18;
+    
+    int total = 3 + 2;
+    
+    if(total ==  5) {
+        printf("Je suis trop fort\n");
+    }
+    
     printf("Age de la noob : %d\n",adrien_age);
+}
+
+int addition(int a, int b) {
+    return a + b;
 }
