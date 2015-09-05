7211b020645b2cb07a49ab17825bd70e26aa13bb
Paul Boiseau <paulboiseau@gmail.com>
2015-09-05 15:47:50 +0200

    open sqlite3 db connection

--- main2.c/main.c
+++ main2.c/main.c
@@ -1,7 +1,24 @@
-#include <stdio.h>
-#include <sqlite3.h>
+#include<stdio.h>
+#include<sqlite3.h>
 
-int main(void)
+int open_db(sqlite3 *db)
 {
-
+    return sqlite3_open("database.db", &db);
 }
+
+int main(int argc, char* argv[])
+{
+    sqlite3 *db;
+    
+    if(open_db(db) == 0) {
+        printf("connexion ouverte");
+    } else {
+        printf("erreur de connexion");
+    }
+    
+    sqlite3_close(db);
+    
+    return 0;
+}
+
+
