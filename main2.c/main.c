ec4fb8b1c4cb1148307aab42f627463847d39368
Paul Boiseau <paulboiseau@gmail.com>
2015-09-19 13:10:30 +0200

    better code

--- main2.c/main.c
+++ main2.c/main.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
-#define RETRAIT 1
-#define DEPOT 2
-#define DEPOTRETRAIT 3
+
+typedef enum
+{
+    RETRAIT, DEPOT, DEPOTRETRAIT
+} Transaction;
 
 int main (void) {
     float solde;
     float somme;
     float somme2;
-    int typeOperation;
-    solde=120.50;
-    typeOperation=DEPOTRETRAIT;
-    somme=35.5;
-    somme2=12;
-    float interet;
-    interet=1.5;
-    if (typeOperation==1) {
+    
+    solde = 120.50;
+    
+    Transaction typeOperation = DEPOTRETRAIT;
+    
+    somme = 35.5;
+    somme2 = 12;
+    
+    float interet = 1.5;
+    
+    if (typeOperation == RETRAIT) {
         solde=solde-somme;
         printf ("Vous avez effectué un retrait, votre solde : %f\n",solde);
-        
     }
-    else if (typeOperation==2) {
+    else if (typeOperation == DEPOT) {
         solde=solde+somme;
         printf ("Vous avez effectué un dépôt, votre solde : %f\n",solde);
+    }
+    else if (typeOperation == DEPOTRETRAIT) {
+        solde = (solde + somme) - somme2;
+        solde = solde + ((solde/100) * interet);
         
+        printf ("Vous avez effecuté un dépôt et un retrait et votre compte à reçu un intérêt, votre solde : %f\n", solde);
     }
-    else if (typeOperation==3){
-        solde=(solde+somme)-somme2;
-        solde=solde+((solde/100)*interet);
-        printf ("Vous avez effecuté un dépôt et un retrait et votre compte à reçu un intérêt, votre solde : %f\n",solde);
-    }
+    
     return 0;
 }
